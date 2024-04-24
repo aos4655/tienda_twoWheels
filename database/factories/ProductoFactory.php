23bbdfd897aff76b5b5427e39c7aaf67bc09934e
aos4655 <aortizsalvador1998@gmail.com>
2024-04-24 10:42:28 +0200

    Generando imagenes mas acordes a los productos.

--- database/factories/ProductoFactory.php
+++ database/factories/ProductoFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Categoria;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Producto>
@@ -17,17 +18,33 @@ class ProductoFactory extends Factory
      */
     public function definition(): array
     {
-        fake()->addProvider(new \Mmo\Faker\PicsumProvider(fake()));
+        /* 
+           1 => 'patinetes' ,
+           2 => 'bicicletas' ,
+           3 =? 'accesorios'
+        */
+        $categoria_id = Categoria::all()->random()->id;
         $stock = random_int(0,50);
         //Esto es para que ponga alguno en no ya que en la bd salen todos si
         ($stock>0 && $stock<=10)? $stock = 0 : $stock;
         return [
             'nombre' => fake()->unique()->words(random_int(2,4), true),
             'descripcion' =>fake()->text(), 
-            'imagen'=>'imgProduct/'.fake()->picsum('public/storage/imgProduct', 640, 480, false), 
+            'imagen'=> $this->obtenerImagenAleatoria($categoria_id), 
             'precio'=>fake()->randomFloat(2,1,9999.99), 
-            'categoria_id' => Categoria::all()->random()->id, 
+            'categoria_id' => $categoria_id, 
             'stock' => $stock, 
         ];
+    }
+    public function obtenerImagenAleatoria($categoria_id){
+        $url = 'https://source.unsplash.com/featured/?';
+        //Obtengo la imagen
+        $imgContenido = file_get_contents($url.($categoria_id == 1 ? 'electric-scooter': 
+                        (($categoria_id == 2) ? 'electric-bike' : 'products')));
+        //Le doy un nombre unico
+        $imgNombre = 'imgProduct/' . uniqid() . '.jpg';
+        //Guardo la imagen en el storage
+        Storage::put($imgNombre, $imgContenido);
+        return $imgNombre;
     }
 }
